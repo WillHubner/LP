fcfe5b03dbf3e43c38e4fd3e966e93dfcbb2169f
Willian Hubner <willian.hubner@hotmail.co.uk>
2022-11-08 20:50:09 -0400

    Add files via upload

---
+++ Aula 01/Aula01.py
@@ -0,0 +1,47 @@
+# import math
+
+# def somar(a, b):
+#     return a + b
+
+# def subtrair(a, b):
+#     return a - b
+
+# def multiplicar(a, b):
+#     return a * b
+
+# def dividir(a, b):
+#     return a / b
+    
+# def elevar(a, b):
+#     return a ** b
+
+# def raiz_quadrada(a):
+#     # return a ** 0.5
+#     return math.sqrt(a)
+
+# print(somar(10,20))
+# print(subtrair(20,10))
+# print(multiplicar(2,3))
+# print(dividir(4,2))
+# print(elevar(2,2))
+# print(raiz_quadrada(4))
+
+# nome = input("digite seu nome")
+# print("Bem vindo "+nome)
+
+# opcao = int(input("digite 0 para abrir, 1 para fechar"))
+
+# if opcao == 0:
+#     print("Você abriu")
+# else: 
+#     print("Voce fechou")
+
+# for i in range(10):
+#     print(i)
+
+segue = True
+i = 0
+while segue:
+    i+=1
+    segue = (i<5)
+    print(i)
